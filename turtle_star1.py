54bd5ba9f8a58dc641db1f1abf0c307701fbd2a6
sunopen777 <162937397+sunopen777@users.noreply.github.com>
2024-03-22 14:14:39 +0900

    Add files via upload

---
+++ turtle_star1.py
@@ -0,0 +1,24 @@
+#터틀 별그리기
+
+import turtle
+import turtle as t
+t.shape("turtle")
+
+t.forward(100)
+t.left(145)
+
+
+t.forward(100)
+t.left(145)
+
+
+t.forward(100)
+t.left(145)
+
+
+t.forward(100)
+t.left(145)
+
+t.forward(100)
+t.left(145)
+
