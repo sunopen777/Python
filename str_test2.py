bd9ebdab05f1760c1ced4e13695f749dc9c7ba54
Undefined <162937397+sunopen777@users.noreply.github.com>
2024-04-22 11:49:40 +0900

    Add files via upload

---
+++ str_test2.py
@@ -0,0 +1,12 @@
+'''a="20240412sunny"
+year=(a[0:4])
+month=(a[4:6])
+day=(a[6:8])
+weather=(a[8:13])
+print("오늘은 " + year + "년 " + month + "월 " + day + "일 입니다\n오늘 날씨는 " + weather + " 입니다")
+'''
+
+b="pithon"
+a=b[0]+"y"+b[2:]
+print(a)
+
