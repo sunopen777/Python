ee9e9969b7deab73124281880c89114413a72e2b
Undefined <162937397+sunopen777@users.noreply.github.com>
2024-04-22 11:46:12 +0900

    Create str.test.py

---
+++ str.test.py
@@ -0,0 +1,14 @@
+n = "Happy Programming!"
+print(n)
+m = n[0:5]
+print(m)
+print(n[-4:-1])
+print(n[6:10])
+print(len(n))
+print(n.upper())
+name = "제 이름은 sunopen777입니다"
+print(len(name))
+name_e = "my name is sunopen777"
+print(len(name_e))
+print(name_e.upper())
+print(name_e.lower())
