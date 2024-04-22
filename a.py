97ab6ec95657f0295c75f026964daa6e36768e4a
Undefined <162937397+sunopen777@users.noreply.github.com>
2024-04-22 11:48:59 +0900

    Create a.py

---
+++ a.py
@@ -0,0 +1,11 @@
+text = "python programming"
+print (text[7:11])
+print (text[-7:-3])
+print (text[7:])
+print (text[:6])
+print (text[-13:-19:-1])
+print (text[7::2])
+major ="Undefined"
+print (major[3])
+print (major[-9:-6])
+print (major[2:8])
